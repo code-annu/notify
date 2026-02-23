ebe67dcbeda9e1f17c303255509484eabe0a4008
Anurag Rajput <code.annu@gmail.com>
2026-02-23 14:20:44 +0530

    Add env.ts

---
+++ backend/src/config/env.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+
+config();
+
+const ENV = {
+  PORT: process.env.PORT || 3000,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET!,
+  JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN!,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!,
+  JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN!,
+};
+
+export default ENV;
