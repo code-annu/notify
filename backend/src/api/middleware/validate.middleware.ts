1615b043360872aba3da783057d2ef5eff1f1b99
Anurag Rajput <code.annu@gmail.com>
2026-02-24 07:58:04 +0530

    add request body validation middleware

---
+++ backend/src/api/middleware/validate.middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction } from "express";
+import { ZodError, ZodObject } from "zod";
+import { Request, Response } from "express";
+import {
+  BadRequestError,
+  InternalServerError,
+} from "../../domain/error/errors";
+
+export const validateRequestBody =
+  (schema: ZodObject<any>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        throw new BadRequestError(
+          error.issues[0]?.message || "Invalid request",
+        );
+      }
+      throw new InternalServerError("Internal server error");
+    }
+  };
